63c3b4cac208d1029886431546aeced89354cf2f
Arpanatha05 <arpanatha7@gmail.com>
2025-04-30 23:44:47 +0530

    ner

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ var typed = new Typed('#element', {
     strings: ['Frontend Devloper', 'Web designer','B-Tech IT student','Youtuber'],
     typeSpeed: 50,
   });
- 
+ //dark and light modde
+ const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark")
+})
